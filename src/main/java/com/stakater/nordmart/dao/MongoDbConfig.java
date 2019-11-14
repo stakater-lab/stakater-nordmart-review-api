c1affaf80895d973148fd2bad93ffcaaf5ed14a1
Nils Ståhl <nils.stahl@Nils-Stahl.local>
2019-11-14 12:07:21 +0100

    db config

---
+++ src/main/java/com/stakater/nordmart/dao/MongoDbConfig.java
@@ -0,0 +1,9 @@
+package com.stakater.nordmart.dao;
+
+
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@EnableMongoRepositories(basePackages = {"com.stakater.nordmart.dao"})
+public class MongoDbConfig {
+
+}
