e4791a47bada51870694468414358e2f7905bd9f
faizanahmad055 <faizan.ahmad55@outlook.com>
2022-06-10 21:37:18 +0200

    Add namespace label in metrics
    Signed-off-by: faizanahmad055 <faizan.ahmad55@outlook.com>

--- src/main/java/com/stakater/nordmart/service/ReviewServiceImpl.java
+++ src/main/java/com/stakater/nordmart/service/ReviewServiceImpl.java
@@ -5,6 +5,7 @@ import com.stakater.nordmart.exception.InvalidDataException;
 import com.stakater.nordmart.model.Review;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tag;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
 import org.apache.commons.lang3.StringUtils;
@@ -35,6 +36,8 @@ public class ReviewServiceImpl implements ReviewService {
     // mode is not final because it is getting value from application.properties
     @Value("${application.mode}")
     String mode;
+    @Value("${application.namespace}")
+    String namespace;
 
     public ReviewServiceImpl(final ReviewRepository repository, final MeterRegistry meterRegistry) {
         this.repository = repository;
@@ -64,7 +67,7 @@ public class ReviewServiceImpl implements ReviewService {
             dummy.add(new Review("444434", "Earthman", "3", "The+watch+is+average+I+think"));
             dummy.add(new Review("444435", "Luke+Skywalker", "4", "My+goto+practice+gadget"));
             try {
-                for(Review review : dummy){
+                for (Review review : dummy) {
                     repository.save(review);
                     // Increment the counter upon adding a new review
                     ratingCounters.get(review.getRating()).increment();
@@ -78,8 +81,11 @@ public class ReviewServiceImpl implements ReviewService {
     }
 
     private Counter buildRatingCounters(final Integer rating) {
+        List<Tag> tags = new ArrayList<>();
+        tags.add(Tag.of("rating", rating.toString()));
+        tags.add(Tag.of("namespace", namespace));
         return Counter.builder("nordmart-review.ratings")    // 2 - create a counter using the fluent API
-                .tag("rating", rating.toString())
+                .tags(tags)
                 .description("Total number of ratings for all product")
                 .register(meterRegistry);
     }
